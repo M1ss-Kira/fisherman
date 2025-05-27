1aaec1534c0aa11b9d5653849c0106a56d12b4f8
M1ss-Kira <saweckra6@gmail.com>
2025-05-27 07:55:09 +0300

    Update First.cs

--- Run/First.cs
+++ Run/First.cs
@@ -1,1 +1,15 @@
+using System;
+using System.Numerics;
 
+public class BigAdder
+{
+    public static void Main(string[] args)
+    {
+        string[] input = Console.ReadLine().Split(' ');
+
+        BigInteger a = BigInteger.Parse(input[0]);
+        BigInteger b = BigInteger.Parse(input[1]);
+
+        Console.WriteLine(a + b);
+    }
+}
